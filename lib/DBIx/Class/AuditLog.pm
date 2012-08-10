b69a8f617394fdd09c79f8584b539941747c49ff
Mark Jubenville <ioncache@gmail.com>
2012-08-10 15:58:27 -0400

    minor perl tidy

--- lib/DBIx/Class/AuditLog.pm
+++ lib/DBIx/Class/AuditLog.pm
@@ -173,11 +173,13 @@ Requires:
 =cut
 
 sub _do_audit {
-    my $self = shift;
+    my $self   = shift;
     my $column = shift;
 
     my $info = $self->column_info($column);
-    return defined $info->{audit_log_column} && $info->{audit_log_column} == 0 ? 0 : 1;
+    return
+        defined $info->{audit_log_column}
+        && $info->{audit_log_column} == 0 ? 0 : 1;
 }
 
 # ABSTRACT: Simple activity audit logging for DBIx::Class
