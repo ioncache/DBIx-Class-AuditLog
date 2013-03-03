3a64e70d3abd9cfd7832686e141ae77032d4c3c2
Mark Jubenville <ioncache@gmail.com>
2013-03-03 06:54:58 -0500

    now enabling quote_names in DBIx::Class::Storage to ensure any reserved words that may be used by the Audit Log schema structure are not conflicting
    Note: this is not backwards compatible, and if for some reason
    quote_names won't work for a specific use case then the Audit Log will
    not work

--- lib/DBIx/Class/Schema/AuditLog.pm
+++ lib/DBIx/Class/Schema/AuditLog.pm
@@ -16,6 +16,21 @@ __PACKAGE__->mk_classdata('audit_log_storage_type');
 
 =head1 DBIx::Class OVERRIDDEN METHODS
 
+=head2 connect
+
+Enable quote_names so that any possible reserved names of columns in the
+Audit Log Schema are protected.
+
+=cut
+
+sub connect {
+    my $self = shift;
+
+    $_[3]->{quote_names} = 1;
+
+    return $self->next::method(@_);
+}
+
 =head2 connection
 
 Overrides the DBIx::Class connection method to create an AuditLog schema.
